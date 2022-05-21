9078c458ecf85120a03ed0120bebf41770bbfbe4
platofff <42435266+platofff@users.noreply.github.com>
2022-05-21 11:20:22 +0300

    Remove unused headers

--- src/calculate.c
+++ src/calculate.c
@@ -1,14 +1,12 @@
 #define _GNU_SOURCE
 #include <cblas.h>
 #include <float.h>
-#include <immintrin.h>
 #include <math.h>
 #include <sched.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 
